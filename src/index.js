0ec2d9cd005fd8d1d89670844ce140773cbc107b
Elvin <ttop34@icloud.com>
2017-09-14 09:16:10 -0500

    prevent default on forms

--- src/index.js
+++ src/index.js
@@ -14,20 +14,58 @@ $(".container").text(`${getLoadMsg()}`);
  */
 const getMovies = require('./getMovies.js');
 
-getMovies().then((movies) => {
-  // console.log('Here are all the movies:');
-  let movie = "";
-  movies.forEach(({title, rating, id}) => {
-     movie += (`id#${id} - ${title} - rating: ${rating}`);
-  });
+function initReq() {
+    getMovies().then((movies) => {
+        // console.log('Here are all the movies:');
+        let movie = "";
+        movies.forEach(({title, rating, id}) => {
+            movie += (`id#${id} - ${title} - rating: ${rating}`);
+        });
 
-      $(".container").html('').append(movie);
-      $('#inputs').show();
+        $(".container").html('').append(movie);
+        $('#inputs').show();
 
 
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    })
+}
+
+initReq();
+
+$(function () {
+
+    $('form').on('submit', function (e) {
+
+        e.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: '/api/movies',
+            data: $('form').serialize(),
+            success: function () {
+                alert('form was submitted');
+            }
+        });
+    });
 });
+
+function showMovies() {
+    let movie = "";
+    getMovies().then((movies) => {
+        movies.forEach(({title, rating, id}) => {
+            movie += (`id#${id} - ${title} - rating: ${rating}`);
+        });
+
+        $(".container").html('').append(movie);
+    })
+}
+
+
+
+
+
+
 
 
